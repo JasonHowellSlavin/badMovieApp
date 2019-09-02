e1eb12d98a4874ff828f12a3d862044ec995e147
Jason Slavin <jslavin@marketo.com>
2019-09-02 13:41:53 -0700

    Starting from scratch

--- client/src/componenets/Dashboard/Dashboard.js
+++ client/src/componenets/Dashboard/Dashboard.js
@@ -1,146 +1,44 @@
 import React, { Component } from 'react';
 import './Dashboard.scss';
 import axios from 'axios';
-import Input from '../Input/Input.js';
-import Responses from '../Responses/Responses.js';
-import Filters from '../Filters/Filters.js';
-import AtAGlance from '../AtAGlance/AtAGlance.js';
-import Sort from '../Sort/Sort.js';
 
 class Dashboard extends Component {
     constructor() {
         super();
         this.state = {
             data: [],
-            activeData: [],
-            activeUrls: [],
-            url: '',
-            filters: {
-                student: '',
-                date: '',
-                targetText: '',
-            },
-            filterMessaging: '',
-        }
-        this.updateQueries = this.updateQueries.bind(this);
-        this.hypoSearchURL = this.hypoSearchURL.bind(this);
-        this.filterByStudent = this.filterByStudent.bind(this);
-        this.filterByDate = this.filterByDate.bind(this);
-        this.filterByTargetText = this.filterByTargetText.bind(this);
-        this.removeDuplicates = this.removeDuplicates.bind(this);
-        this.clearFilters = this.clearFilters.bind(this);
-        this.sortHook = this.sortHook.bind(this);
-    }
-
-    // Functionality methods
-    updateQueries (value, name) {
-        this.setState({[name]: value});
-    }
-
-    removeDuplicates(currentData) {
-        let duplicatHash = {};
-
-        return currentData.filter((item) => {
-            return duplicatHash.hasOwnProperty(item.id) ? false : duplicatHash[item.id] = true;
-        });
-    }
-
-
-    // Filter methods
-    filterByStudent (studentName) {
-        let correctName = studentName.indexOf('acct:') > -1 ? studentName : 'acct:' + studentName;
-        let filteredActiveData = this.state.activeData.slice();
-
-        let dataFiltered = filteredActiveData.filter((item, index, array) => {
-            return item.user === correctName;
-        })
-
-        if (dataFiltered.length > 0) {
-            this.setState({activeData: dataFiltered});
-        } else {
-            this.setState({filterMessaging: 'Sorry, no data matched that filter'});
         }
     }
 
-    filterByDate (date) {
-        let dateRegEx = /([0-9]{4}-[0-9]{2}-[0-9]{2})/g;
-        let filteredActiveData = this.state.activeData.slice();
-
-        let dateFiltered =  filteredActiveData.filter((item, index, array) => {
-            return item.created.match(dateRegEx)[0] === date;
-        })
-
-        if (date !== date.match(dateRegEx)[0]) {
-            this.setState({filterMessaging: 'Sorry you did not enter the date correctly, format should be YYYY-MM-DD'});
-        } else if (dateFiltered.length <= 0) {
-            this.setState({filterMessaging: 'It appears there are annotations matching the date you chose'});
-        } else {
-            this.setState({activeData: dateFiltered});
-        }
-    }
-
-    filterByTargetText (targetText) {
-        let filteredActiveData = this.state.activeData.slice();
-
-        let textFiltered = filteredActiveData.filter((item, index, array) => {
-            if (item.target[0].selector) {
-                let lowerCaseExact = item.target[0].selector[3].exact.toLowerCase();
-                return lowerCaseExact.indexOf(targetText.toLowerCase()) > -1;
-            } else {
-                return false;
-            }
-        })
-
-        if (textFiltered.length <= 0) {
-            this.setState({filterMessaging: 'It appears the target text does not appear'});
-        } else {
-            this.setState({activeData: textFiltered});
-        }
-    }
-
-    clearFilters () {
-        let unfilteredData = this.state.data;
-        this.setState({activeData: unfilteredData});
-    }
-
-    // Sort methods
-    sortHook (methodReturn) {
-        this.setState({activeData: methodReturn})
-    }
-
-
-    // API Requests
-    hypoSearchURL () {
-      //http://teaching.lfhanley.net/english528sp18/texts/edna-st-vincent-millay/
-      // http://teaching.lfhanley.net/english528sp18/texts/philip-levine-they-feed-they-lion-1972/
-    axios.request({
-            url: `api/search/url`,
-            method: 'get',
-            params: {
-              queriedURL: this.state.url
-            }
-        }).then((response) => {
-            console.log('Response:', response);
-            let stateData = this.state.data.slice();
-            let currentActiveUrls = this.state.activeUrls.slice();
-
-            currentActiveUrls.push(this.state.url);
-
-            response.data.forEach(item => stateData.push(item));
-
-            let cleanedStateData = this.removeDuplicates(stateData);
-
-            this.setState({
-                data: cleanedStateData,
-                url: '',
-                activeUrls: currentActiveUrls,
-                activeData: cleanedStateData
-            });
-            console.log(stateData, 'stateData');
-        }).catch((error) => {
-            console.log('error', error);
-        });
-    }
+    // hypoSearchURL () {
+    //     axios.request({
+    //         url: `api/search/url`,
+    //         method: 'get',
+    //         params: {
+    //           queriedURL: this.state.url
+    //         }
+    //     }).then((response) => {
+    //         console.log('Response:', response);
+    //         let stateData = this.state.data.slice();
+    //         let currentActiveUrls = this.state.activeUrls.slice();
+    //
+    //         currentActiveUrls.push(this.state.url);
+    //
+    //         response.data.forEach(item => stateData.push(item));
+    //
+    //         let cleanedStateData = this.removeDuplicates(stateData);
+    //
+    //         this.setState({
+    //             data: cleanedStateData,
+    //             url: '',
+    //             activeUrls: currentActiveUrls,
+    //             activeData: cleanedStateData
+    //         });
+    //         console.log(stateData, 'stateData');
+    //     }).catch((error) => {
+    //         console.log('error', error);
+    //     });
+    // }
 
     // Lifecycle Hooks
     componentDidMount() {
@@ -151,69 +49,8 @@ class Dashboard extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-title">Hypothes.is Dashboard</h1>
+                    <h1 className="App-title">Bad Movie App</h1>
                 </header>
-                <section className="interface">
-                    <div className="user-inputs">
-                        <h3> Enter a URL below to see annotations from that url.
-                        If you would like to check multiple urls, simply add them one at a
-                        time and click submit</h3>
-                        <div>
-                            <form id="form">
-                                <Input
-                                    url={this.state.url}
-                                    labelInputName={'url'}
-                                    labelText={'URL'}
-                                    inputType={'text'}
-                                    inputValue={this.state.url}
-                                    updateQueries={this.updateQueries}
-                                />
-                            </form>
-                            <button onClick={this.hypoSearchURL}>Submit</button>
-                        </div>
-                        <div className="active-urls">
-                            <ul>
-                                <h4>Data sourced from the following URLs:</h4>
-                                {this.state.activeUrls.map( item => <li>{item}</li>)}
-                            </ul>
-                        </div>
-                    </div>
-                    <section className="filters">
-                        <Filters
-                            filterByStudent={this.filterByStudent}
-                            filterByDate={this.filterByDate}
-                            filterByTargetText={this.filterByTargetText}
-                            filterMessaging={this.state.filterMessaging}
-                            clearFilters={this.clearFilters}
-                         />
-                    </section>
-                </section>
-                <section className="visualization">
-                    <div className="response-browser">
-                        <section className="responses">
-                            <h2> Annotations </h2>
-                            <p>Currently viewing {this.state.activeData.length ? this.state.activeData.length : 0} of {this.state.data.length ? this.state.data.length : 0} total Annotations</p>
-                            <div className="sort-wrapper">
-                                <Sort
-                                    data={this.state.activeData}
-                                    sortHook={this.sortHook}
-                                />
-                            </div>
-                            <Responses
-                                activeData={this.state.activeData}
-                                totalData={this.state.data}
-                            />
-                        </section>
-                    </div>
-                    <div className="graphs">
-                        <h2>At a Glance Statistics </h2>
-                        <p>Currently viewing statistics from {this.state.activeData.length ? this.state.activeData.length : 0} annotations</p>
-                        <AtAGlance className="at-a-glance-wrapper"
-                            activeData={this.state.activeData}
-                            totalData={this.state.data}
-                         />
-                    </div>
-                </section>
           </div>
         );
     }
--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -4,17 +4,17 @@ exports.canned_response = function(req, res, next) {
     res.send({text: 'This is a canned response'});
 };
 
-exports.url_only_search = function(req, res, next) {
-    axios.request({
-      url: process.env.HYPOTHESIS_SEARCH_URL,
-      params: {
-           url: req.query.queriedURL,
-           password: process.env.API_KEY,
-           limit: 200},
-        method: 'get',
-    }).then((axios_res) => {
-        res.send(axios_res.data.rows);
-    }).catch((error) => {
-        console.log('error', error);
-    });
-};
+// exports.url_only_search = function(req, res, next) {
+//     axios.request({
+//       url: process.env.HYPOTHESIS_SEARCH_URL,
+//       params: {
+//            url: req.query.queriedURL,
+//            password: process.env.API_KEY,
+//            limit: 200},
+//         method: 'get',
+//     }).then((axios_res) => {
+//         res.send(axios_res.data.rows);
+//     }).catch((error) => {
+//         console.log('error', error);
+//     });
+// };
